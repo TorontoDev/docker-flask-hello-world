2050bef77e822f5508f01fdb48c3dbba29c4b614
HardeepS <hardeep.salh@empire.ca>
2020-07-25 12:43:15 -0400

    First commit : Docker setup + hot reloading

---
+++ app.py
@@ -0,0 +1,20 @@
+# app.py - a minimal flask api using flask_restful
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class MyHelloWorld(Resource):
+    def get(self):
+        return {'data': 'This is coming from my hello world.'}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(MyHelloWorld, '/test')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
